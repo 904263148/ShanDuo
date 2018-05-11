f9c985743115768bb8a578de075181c1e0d63276
cq173 <1322127081@qq.com>
2018-05-11 15:49:14 +0800

    20180511 15：41 高德地图定位搜索集成

--- app/src/main/java/com/yapin/shanduo/ui/activity/AddactivityActivity.java
+++ app/src/main/java/com/yapin/shanduo/ui/activity/AddactivityActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +19,7 @@ import com.yapin.shanduo.app.ShanDuoPartyApplication;
 import com.yapin.shanduo.presenter.Addactivitypresenter;
 import com.yapin.shanduo.ui.contract.AddactivityContract;
 import com.yapin.shanduo.utils.DateTimePickDialogUtil;
+import com.yapin.shanduo.utils.StartActivityUtil;
 import com.yapin.shanduo.utils.ToastUtil;
 
 import butterknife.BindView;
@@ -58,7 +61,7 @@ public class AddactivityActivity extends BaseActivity implements AddactivityCont
     String manNumber;
     String womanNumber;
     String activityType;
-
+    String textlonlat;
 
     private Addactivitypresenter presenter;
     private Context context;
@@ -83,7 +86,7 @@ public class AddactivityActivity extends BaseActivity implements AddactivityCont
                 AlertDialog.Builder builder1 = new AlertDialog.Builder(activity);
                 final String[] emotion = {"我请客", "AA制","男的AA，美女随意"};
                 //    设置一个单项选择下拉框
-                builder1.setSingleChoiceItems(emotion, 0, new DialogInterface.OnClickListener()
+                builder1.setSingleChoiceItems(emotion, 4, new DialogInterface.OnClickListener()
                 {
                     @Override
                     public void onClick(DialogInterface dialog, int which)
@@ -116,8 +119,6 @@ public class AddactivityActivity extends BaseActivity implements AddactivityCont
             public void onClick(View view) {
                 DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(AddactivityActivity.this, null);
                 dateTimePicKDialog.dateTimePicKDialog(tv_add_Deadline, 0, true);
-//                activityCutoffTime =tv_add_Deadline.getText().toString().trim();
-//                Toast.makeText(context, activityCutoffTime+"", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -126,13 +127,36 @@ public class AddactivityActivity extends BaseActivity implements AddactivityCont
             public void onClick(View view) {
                 DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(AddactivityActivity.this, null);
                 dateTimePicKDialog.dateTimePicKDialog(tv_add_starttime, 0, true);
-//                activityStartTime=tv_add_starttime.getText().toString().trim();
-//                Toast.makeText(context, activityStartTime+"", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv_add_place.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(activity ,MapGaodeActivity.class);
+                startActivityForResult(intent , 1);
             }
         });
 
         initView();
     }
+    //获取定位选择的数据
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+
+//            data.getStringExtra("textTitle");
+            setText(data.getStringExtra("textTitle"));
+            textlonlat = data.getStringExtra("textlonlat");
+            Log.i("test","/n地址是："+tv_add_place+"/n经纬度是："+textlonlat);
+        }
+    }
+    //绑定定位选择的数据
+    public void setText(String data){
+        tv_add_place.setText(data);
+    }
+
 
     @Override
     public void initView() {
@@ -144,17 +168,20 @@ public class AddactivityActivity extends BaseActivity implements AddactivityCont
 //        activityType ,activityStartTime, activityAddress ,mode, manNumber ,womanNumber, remarks ,activityCutoffTime, lon, lat
         switch (view.getId()) {
             case R.id.but_add_Releaseactivities:
+
+                String []ary = textlonlat.split("\\,");
+                lat=ary[0];
+                lon=ary[1];
+                Log.i("test","/n经度："+lon+"/n纬度："+lat);
                 activityType = et_add_title.getText().toString().trim();
                 womanNumber = et_add_numberofgirls.getText().toString().trim();
                 manNumber = et_add_Numberofboys.getText().toString().trim();
-                activityAddress = "长沙县金科时代中心";
+
+                activityAddress = tv_add_place.getText().toString().trim();
                 remarks = et_add_Remarks.getText().toString().trim();
-                lon="113.914619";
-                lat="30.611842";
+
                 activityCutoffTime =tv_add_Deadline.getText().toString().trim();
-                Toast.makeText(context, activityCutoffTime+"", Toast.LENGTH_SHORT).show();
                 activityStartTime=tv_add_starttime.getText().toString().trim();
-                Toast.makeText(context, activityStartTime+"", Toast.LENGTH_SHORT).show();
 
                 presenter.addactivity(activityType,activityStartTime,activityAddress,mode,manNumber,womanNumber,remarks,activityCutoffTime,lon,lat);
                 break;
